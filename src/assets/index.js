844b2c74f4aa6cf9472a94baa52d274740de347c
ThembaMasilela <21062120+ThembaMasilela@users.noreply.github.com>
2024-10-22 10:37:52 +0200

    fix logo error

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,56 +1,57 @@
-import nairobi from './backgrounds/nairobi.png';
-import whiteabstract from './backgrounds/white-abstract.png';
-import bghero from './backgrounds/bghero.jpg';
+import nairobi from "./backgrounds/nairobi.png";
+import whiteabstract from "./backgrounds/white-abstract.png";
+import bghero from "./backgrounds/bghero.jpg";
 
-import logo from './logo/logo-black.png';
-import logotext from './logo/logo-text-black.png';
+import logo from "./logo/logo-black.png";
+import logotext from "./logo/logo-text-black.png";
 
-import dev from './personal/developer-dark.svg';
+import dev from "./personal/developer-dark.svg";
 
-import backend from './icons/backend.png';
-import ux from './icons/ux.png';
-import frontend from './icons/frontend.png';
-import prototyping from './icons/prototyping.png';
-import github from './icons/github.png';
-import close from './icons/close.png';
-import download from './icons/download.png';
-import downloadHover from './icons/downloadHover.png';
-import pineapple from './icons/pineapple.png';
-import pineappleHover from './icons/pineappleHover.png';
-import menu from './icons/menu.png';
-import resume from './icons/resume.png';
-import send from './icons/send.png';
-import sendHover from './icons/sendHover.png';
+import backend from "./icons/backend.png";
+import ux from "./icons/ux.png";
+import frontend from "./icons/frontend.png";
+import prototyping from "./icons/prototyping.png";
+import github from "./icons/github.png";
+import close from "./icons/close.png";
+import download from "./icons/download.png";
+import downloadHover from "./icons/downloadHover.png";
+import pineapple from "./icons/pineapple.png";
+import pineappleHover from "./icons/pineappleHover.png";
+import menu from "./icons/menu.png";
+import resume from "./icons/resume.png";
+import send from "./icons/send.png";
+import sendHover from "./icons/sendHover.png";
 
-import css from './tech/css.png';
-import figma from './tech/figma.png';
-import git from './tech/git.png';
-import html from './tech/html.png';
-import javascript from './tech/javascript.png';
-import nodejs from './tech/nodejs.png';
-import reactjs from './tech/reactjs.png';
-import redux from './tech/redux.png';
-import tailwind from './tech/tailwind.png';
-import typescript from './tech/typescript.png';
-import postgresql from './tech/postgresql.png';
-import csharp from './tech/csharp.png';
-import dotnet from './tech/dotnet.png';
-import bootstrap from './tech/bootstrap.png';
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import postgresql from "./tech/postgresql.png";
+import csharp from "./tech/csharp.png";
+import dotnet from "./tech/dotnet.png";
+import bootstrap from "./tech/bootstrap.png";
 
-import takealot from './company/takealot.png';
-import capitec from './company/capitec.png';
-import briefcase from './company/briefcase.png';
-import altron from './company/altron.png';
-import africanbank from './company/africanbank.png';
+import takealot from "./company/takealot.png";
+import capitec from "./company/capitec.png";
+import briefcase from "./company/briefcase.png";
+import altron from "./company/altron.png";
+import africanbank from "./company/africanbank.png";
+import fnb from "./company/fnb.png";
 
-import komikult from './projects/komikult.png';
-import leaderboard from './projects/leaderboard.png';
-import math from './projects/math-magicians.png';
-import movie from './projects/movie-metro.png';
-import nyeusi from './projects/nyeusi.png';
-import space from './projects/space-hub.png';
+import komikult from "./projects/komikult.png";
+import leaderboard from "./projects/leaderboard.png";
+import math from "./projects/math-magicians.png";
+import movie from "./projects/movie-metro.png";
+import nyeusi from "./projects/nyeusi.png";
+import space from "./projects/space-hub.png";
 
-import myresume from './personal/Resume_Themba Masilela.pdf';
+import myresume from "./personal/Resume_Themba Masilela.pdf";
 
 export {
   myresume,
@@ -98,5 +99,6 @@ export {
   altron,
   briefcase,
   capitec,
-  takealot
+  takealot,
+  fnb,
 };
